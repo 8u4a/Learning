f348a232284af6bb12678ea2c537e599539b446c
Irene <marinescuirina88@gmail.com>
2018-02-07 22:49:31 +0200

    my first git commit

---
+++ 07-02-2018/returnJsonobject.js
@@ -0,0 +1,24 @@
+/*fa o functie care intoarce un obiect JSON care are 3 proprietati:
+
+nume, prenume, varsta.
+/algoritm
+ne trebuie o functie getDetails()
+numele, prenumele si varsta
+Vasilica: {
+  nume: 'Ion',
+  prenume: 'Victor',
+  varsta: 30
+}
+
+*/
+
+function getDetails() {
+  return Vasilica
+}
+var Vasilica = {
+  nume: 'Ion',
+  prenume: 'Victor',
+  varsta: 30
+}
+var details = getDetails();
+console.log(details)
