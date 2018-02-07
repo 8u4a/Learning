d8d60e87cd67d6d3d62329cbf7e0f79d396def09
buha <marinescuirina88@gmail.com>
2018-02-07 23:54:09 +0200

    secondexercise

---
+++ 07-02-2018/returnLength.js
@@ -0,0 +1,15 @@
+/*
+
+functie care sa priemasca un string ca parametru si sa intoarca numaru de caractere din string.
+
+*/
+
+function getInfo(cipic)
+{
+  console.log('cipic este: ', cipic)
+  return cipic.length;
+
+}
+var lista = getInfo('mere');
+//console.log(getInfo())
+console.log(lista)
